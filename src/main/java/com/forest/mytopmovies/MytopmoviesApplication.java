1bceea496edcfb505571adc63035290220a9d5ac
forest <azureforestryan@gmail.com>
2021-12-05 10:45:23 +0000

    initial commit

---
+++ src/main/java/com/forest/mytopmovies/MytopmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.forest.mytopmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytopmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytopmoviesApplication.class, args);
+	}
+
+}
